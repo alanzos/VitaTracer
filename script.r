5a842ebe2e492315bd58f27d0f25b0087a427a0d
Andrés Lanzós <alanzos@users.noreply.github.com>
2023-05-14 23:00:38 +0200

    Added overview and draft of individual analyte plot

--- script.r
+++ script.r
@@ -4,6 +4,9 @@ library(ggplot2)
 library(stringi)
 library(DT)
 library(plotly)
+library(ComplexHeatmap)
+library(InteractiveComplexHeatmap)
+library(viridis)
 
 options(encoding = 'UTF-8')
 
@@ -14,39 +17,34 @@ jscode_upload_msg <- " Shiny.addCustomMessageHandler('upload_msg', function(msg)
 
 ui <- fluidPage(lang = "es",
   tags$script(HTML(jscode_upload_msg)),
-  titlePanel(title = span(img(src = "logo.png", height = 50)),windowTitle = "MICROWD Diversificación"),
+  titlePanel(title = span(img(src = "logo_vitatracer.JPG", height = 50)),windowTitle = "VitaTracer"),
   sidebarLayout(
     position = "left",
     sidebarPanel(
-      h5("Inspecciona la diversificación de tus inversiones en MICROWD según varios factores y métricas."),
-      h5("Usa el excel de ejemplo o carga el tuyo, que puedes descargar en la pestaña de \"Inversiones\" de la sección \"Perfil\" dentro de MICROWD."),
-      # br(),
-      radioButtons("select_dataset",label="Seleccionar excel", choices=c("Excel de ejemplo","Cargar excel"),inline = F, selected="Excel de ejemplo"),
-      conditionalPanel("input.select_dataset=='Cargar excel'",
+      h5("Explore evolution of your lab results (like blood and urine) over time, and detect which values are outside of the reference."),
+      h5("Use the example data or load your own file following the format of ", a("this template", href="https://www.linkedin.com/in/andreslanzos", .noWS = "after"), "."),
+      radioButtons("select_dataset",label="Select a choice:", choices=c("Example data","Upload data"),inline = F, selected="Example data"),
+      conditionalPanel("input.select_dataset=='Upload data'",
                        fileInput(inputId = "fileUpload",
-                                  label = "Cargar excel .xls",
+                                  label = "Upload data",
                                   multiple = FALSE,
-                                  buttonLabel = "Cargar",
-                                  placeholder = "Ningun excel cargado",
+                                  buttonLabel = "Upload",
+                                  placeholder = "No data uploaded",
                                   accept = c(".xls")),                                     
                        uiOutput('ui.action') ),
-      radioButtons(inputId = "variable_x",
-                   label = "Factor:",
-                   choices = sort(c("Emprendedora","Equipo local","Moneda local","Riesgo de divisa","Periodicidad de pago","Municipio","País","Sector del negocio","Status","Tipo de negocio"))),
-      radioButtons(inputId = "variable_y",
-                   label = "Métrica:",
-                   choices = c("Número de inversiones","Cantidad invertida €")),
-      h5("Esta applicación web no ha sido desarrollada por MICROWD, sino que es un proyecto independiente. Autor: ", a("Andrés Lanzós.", href="https://www.linkedin.com/in/andreslanzos"))
+      h5("Contact: ", a("Andrés Lanzós.", href="https://www.linkedin.com/in/andreslanzos"))
       
     ),
     mainPanel(
       tabsetPanel(type = "tabs",
-                  tabPanel("Grafico",
-                           plotlyOutput("plot", height = 600)
+                  tabPanel("Overview",
+                           plotOutput("plot_overview", height = 1000, width = 1200)
                   ),
-                  tabPanel("Tabla", 
-                           DT::dataTableOutput(outputId = "table"),
-                           downloadButton("downloadtable", "Descargar tabla")
+                  tabPanel("Individual values",
+                           radioButtons(inputId = "individual_analyte",
+                                        label = "Analyte:",
+                                        choices = c("Blood - Hemogram white - Leucocytes (10^3/μl)")),
+                           plotOutput("plot_individual", height = 600)
                   )
       )
     )
@@ -56,85 +54,135 @@ ui <- fluidPage(lang = "es",
 server <- function(input, output,session) {
   
   observeEvent(input$fileUpload, {
-    session$sendCustomMessage("upload_msg", "Cargando excel")
+    session$sendCustomMessage("upload_msg", "Uploading data")
   })
   
   d <- reactive({
     
-    variable_x=gsub("País","Pais",gsub(" ","_",input$variable_x))
-    variable_y=input$variable_y
-    if (input$select_dataset=="Excel de ejemplo") {file=readxl::read_xls("data/inversiones_en_curso.xls",1)
+   if (input$select_dataset=="Example data") {
+      file=readxl::read_xlsx("data/summary.xlsx",1)
     } else {
       validate(
-        need(!is.null(input$fileUpload), "Carga un excel con tus inversiones o selecciona el excel de ejemplo")
+        need(!is.null(input$fileUpload), "Error: please upload your data or choose the example one.")
       )
       file=input$fileUpload
-      file=readxl::read_xls(as.character(file$datapath),1)
+      file=readxl::read_xlsx(as.character(file$datapath),1)
     }
-    names(file)=c("Emprendedora","Pais","Municipio","Equipo_local","Sector_del_negocio","Tipo_de_negocio","Status","Fecha_de_inversion","Fecha_de_cheque","Inversion","Dinero_nuevo","Reinversion","Tipo_de_cambio_inversion","Cantidad_recuperada_Moneda_Local","Tipo_de_cambio_ultimo","Cantidad_recuperada","Fecha_ultimo_pago","Moneda_local","Riesgo_de_divisa","Periodicidad_de_pago")
-    file$Inversion=as.numeric(file$Inversion)
-    file$Emprendedora=stri_trans_totitle(file$Emprendedora)
-    file$Municipio=stri_trans_totitle(file$Municipio)
-    table_to_plot = file %>%
-      dplyr::group_by_at(variable_x) %>%
-      dplyr::summarise(Frequency=length(Inversion),
-                       Quantity=sum(Inversion))
-    if (variable_y=="Cantidad invertida €") {variable_y2="Quantity"} else {variable_y2="Frequency"}
-    table_to_plot[,c(variable_x,variable_y2)]
-  })
-  
-  
-  d2 <- reactive({
-    variable_x=gsub("País","Pais",gsub(" ","_",input$variable_x))
-    variable_x
-  })
-  d3 <- reactive({
-    variable_y=input$variable_y
-    variable_y
-  })
-  
-  
-  
-  output$plot <- renderPlotly({
-    table_to_plot=d()
-    variable_x=d2()
-    variable_y=d3()
-    if (variable_y=="Cantidad invertida €") {variable_y2="Quantity"} else {variable_y2="Frequency"}
-    my_gg=ggplot(data = table_to_plot, aes(x=get(variable_x), y=get(variable_y2))) +
-      geom_col()  +
-      theme_classic() +
-      theme(axis.text.y = element_text(color="black"),
-            axis.text.x = element_text(color="black")) +
-      xlab(gsub("Pais","País",gsub("_"," ",variable_x))) +
-      ylab(variable_y) +
-      coord_flip()
-    ggplotly(my_gg,tooltip = c("text"))
-  })
-
-  
-  output$table <- DT::renderDataTable({
-    file=d()
-    colnames(file)=c(gsub("Pais","País",gsub("_"," ",d2())),d3())
-    file
-  },
-  rownames = FALSE,
-  options = list(
-    language = list(url = '//cdn.datatables.net/plug-ins/1.10.11/i18n/Spanish.json'),
-    search = list(regex = TRUE)))
-
-  
-  output$downloadtable <- downloadHandler(
-    filename = function() {
-      paste('tabla_resumen_de_inversiones', '.csv', sep='')
-    },
-    content = function(file) {
-      summary_data_temp=d()
-      colnames(summary_data_temp)=c(gsub("Pais","País",gsub("_"," ",d2())),d3())
-      summary_data_temp
-      write.csv(summary_data_temp, file,row.names = F)
-    }
-  )
+    file <- file %>% 
+      dplyr::arrange(Sample, Analyte, Unit)  %>% 
+      dplyr::mutate(
+        Analyte = paste0(Sample, " - ", Category, " - ", Analyte, " (", Unit, ")"),
+        Min = as.numeric(Min),
+        Measure = as.numeric(Measure),
+        Max = as.numeric(Max),
+        Date = as.Date.character(Date),
+        Date = as.character(Date),
+        Color = ifelse(Measure < Min, "Low",
+                       ifelse(Measure > Max, "High",
+                              "Good"))
+      )
     
+    # file$Emprendedora=stri_trans_totitle(file$Emprendedora)
+    file
+  })
+  
+  
+  # d2 <- reactive({
+  #   variable_x=gsub("País","Pais",gsub(" ","_",input$variable_x))
+  #   variable_x
+  # })
+  d3 <- reactive({
+    individual_analyte=input$individual_analyte
+    individual_analyte
+  })
+  
+  
+  
+  output$plot_overview <- renderPlot({
+    file=d()
+    table_for_heatmap <- file %>%
+      dplyr::arrange(Date, Analyte) %>%
+      tidyr::pivot_wider(id_cols = Analyte, names_from = Date, values_from = Color) %>%
+      dplyr::arrange(Analyte) %>%
+      tibble::column_to_rownames("Analyte") %>%
+      as.matrix()
+    
+    names_rows <- row.names(table_for_heatmap)
+    
+    Sample <- sapply(names_rows, function(x) {
+      strsplit(x, " - ")[[1]][[1]]
+    })
+    Sample_colors <- viridis::viridis(length(unique(Sample)))
+    names(Sample_colors) <- unique(sort(Sample))
+    
+    Category <- sapply(names_rows, function(x) {
+      strsplit(x, " - ")[[1]][[2]]
+    })
+    Category_colors <- viridis::viridis(length(unique(Category)))
+    names(Category_colors) <- unique(sort(Category))
+    
+    
+    
+    color_scale <- c("Green", "Red", "Orange", "White")
+    
+    row_ha <- rowAnnotation(
+      "Sample" = Sample,
+      "Category" = Category,
+      col = list(
+        "Sample" = Sample_colors,
+        "Category" = Category_colors
+      ),
+      # annotation_name_side = "top",
+      annotation_name_rot = 45,
+      annotation_legend_param = list(Sample = list(title_gp = gpar(fontsize = 18, fontface = "bold"), 
+                                                   labels_gp = gpar(fontsize = 14)),
+                                     Category = list(title_gp = gpar(fontsize = 18, fontface = "bold"), 
+                                                   labels_gp = gpar(fontsize = 14)))
+    )
+    
+    
+    ht <- ComplexHeatmap::Heatmap(table_for_heatmap,
+                                  name = "Value",
+                                  col = color_scale,
+                                  cluster_rows = F,
+                                  cluster_columns = F,
+                                  na_col = "White",
+                                  row_title = "",
+                                  row_title_side = "left",
+                                  row_names_side = "left",
+                                  row_names_max_width = max_text_width(
+                                    rownames(table_for_heatmap)
+                                  ),
+                                  column_title = "Date",
+                                  column_title_side = "bottom",
+                                  column_names_side = "bottom",
+                                  left_annotation = row_ha,
+                                  column_names_rot = 45, 
+                                  # width = unit(15, "cm"),
+                                  heatmap_legend_param = list(title_gp = gpar (fontsize = 18, fontface = "bold"),
+                                                              labels_gp = gpar(fontsize = 14))
+    )
+    
+    ht <- draw(ht, merge_legends = TRUE)
+    
+  })
+  
+  output$plot_individual <- renderPlot({
+    individual_analyte = d3()
+    file <- d() %>% 
+      dplyr::filter(Analyte == individual_analyte) %>% 
+      dplyr::mutate(Color2 = c(Color[2:length(Color)],Color[1]))
+    print(nrow(file))
+    ggplot(data = file,
+           mapping = aes(
+             x = Date,
+             y = Measure,
+           )) +
+      ylab(individual_analyte) +
+      geom_ribbon(data = file, mapping = aes(x=Date, y=Measure, ymax=Max, ymin=Min, group = 1), fill = "green", alpha = 0.2) +
+      geom_line(mapping = aes(group = 1, color = Color2)) +
+      geom_point(mapping = aes(color = Color), size = 5)
+  })
 }
 
 shinyApp(ui = ui, server = server)
