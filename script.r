311aec1f9cbbfdc94de6559dd121804f627cd228
Andrés Lanzós <alanzos@users.noreply.github.com>
2023-05-15 22:39:49 +0200

    Working version with better heatmap

--- script.r
+++ script.r
@@ -38,12 +38,17 @@ ui <- fluidPage(lang = "es",
     mainPanel(
       tabsetPanel(type = "tabs",
                   tabPanel("Overview",
+                           br(),
+                           selectInput("sorting_variable",
+                                          label="Sort the plot below by:",
+                                          choices=c("Sample", "Category", "Analyte", "Unit")),
                            plotOutput("plot_overview", height = 1000, width = 1200)
                   ),
                   tabPanel("Individual values",
-                           radioButtons(inputId = "individual_analyte",
-                                        label = "Analyte:",
-                                        choices = c("Blood - Hemogram white - Leucocytes (10^3/μl)")),
+                           br(),
+                           selectizeInput("individual_analyte",
+                                          label="Search and select an analyte to display:",
+                                          choices=NULL),
                            plotOutput("plot_individual", height = 600)
                   )
       )
@@ -71,7 +76,8 @@ server <- function(input, output,session) {
     file <- file %>% 
       dplyr::arrange(Sample, Analyte, Unit)  %>% 
       dplyr::mutate(
-        Analyte = paste0(Sample, " - ", Category, " - ", Analyte, " (", Unit, ")"),
+        # Analyte = paste0(Sample, " - ", Category, " - ", Analyte, " (", Unit, ")"),
+        Full_Name = paste0(Sample, " - ", Category, " - ", Analyte, " - ", Unit),
         Min = as.numeric(Min),
         Measure = as.numeric(Measure),
         Max = as.numeric(Max),
@@ -79,12 +85,25 @@ server <- function(input, output,session) {
         Date = as.character(Date),
         Color = ifelse(Measure < Min, "Low",
                        ifelse(Measure > Max, "High",
-                              "Good"))
+                              "Good")),
+        Color = factor(Color, levels = c("High", "Good", "Low", NA))
       )
+    
+    list_names_for_search <- unique(sort(unlist(file$Full_Name)))
+    list_names_for_search <- list_names_for_search[list_names_for_search!=""]
+    # list_names_for_search <- list_names_for_search[grepl("\\D", list_names_for_search)]
+    updateSelectizeInput(session, "individual_analyte",
+                         label = NULL,
+                         selected="Blood - Hemogram white - Leucocytes (10^3/μl)",
+                         # selected="Urine - Urine - Nitrites (NA)",
+                         choices = list_names_for_search,
+                         server = TRUE)
+    print("done")
     
     # file$Emprendedora=stri_trans_totitle(file$Emprendedora)
     file
   })
+  
   
   
   # d2 <- reactive({
@@ -100,11 +119,13 @@ server <- function(input, output,session) {
   
   output$plot_overview <- renderPlot({
     file=d()
+    sorting_variable = input$sorting_variable
     table_for_heatmap <- file %>%
       dplyr::arrange(Date, Analyte) %>%
-      tidyr::pivot_wider(id_cols = Analyte, names_from = Date, values_from = Color) %>%
-      dplyr::arrange(Analyte) %>%
-      tibble::column_to_rownames("Analyte") %>%
+      tidyr::pivot_wider(id_cols = c(Full_Name, Analyte, Sample, Unit, Category), names_from = Date, values_from = Color) %>%
+      dplyr::arrange(get(sorting_variable), Sample, Analyte, Category, Unit) %>%
+      tibble::column_to_rownames("Full_Name") %>%
+      dplyr::select(-c(Analyte, Sample, Unit, Category)) %>% 
       as.matrix()
     
     names_rows <- row.names(table_for_heatmap)
@@ -121,41 +142,99 @@ server <- function(input, output,session) {
     Category_colors <- viridis::viridis(length(unique(Category)))
     names(Category_colors) <- unique(sort(Category))
     
+    Analyte <- sapply(names_rows, function(x) {
+      strsplit(x, " - ")[[1]][[3]]
+    })
+    
+    Unit <- sapply(names_rows, function(x) {
+      strsplit(x, " - ")[[1]][[4]]
+    })
+    Unit_colors <- viridis::viridis(length(unique(Unit)))
+    names(Unit_colors) <- unique(sort(Unit))
     
     
-    color_scale <- c("Green", "Red", "Orange", "White")
+    
+    
+    color_scale <- c("#CD0000", "#3e9e1f", "#ff3300", "#ffffff")
+    names(color_scale) <- c("High", "Good", "Low", NA)
+    
+    # row_ha <- rowAnnotation(
+    #   "Sample" = Sample,
+    #   "Category" = Category,
+    #   col = list(
+    #     "Sample" = Sample_colors,
+    #     "Category" = Category_colors
+    #   ),
+    #   # annotation_name_side = "top",
+    #   annotation_name_rot = 45,
+    #   annotation_legend_param = list(Sample = list(title_gp = gpar(fontsize = 18, fontface = "bold"), 
+    #                                                labels_gp = gpar(fontsize = 14)),
+    #                                  Category = list(title_gp = gpar(fontsize = 18, fontface = "bold"), 
+    #                                                labels_gp = gpar(fontsize = 14)))
+    # )
     
     row_ha <- rowAnnotation(
-      "Sample" = Sample,
-      "Category" = Category,
-      col = list(
-        "Sample" = Sample_colors,
-        "Category" = Category_colors
-      ),
-      # annotation_name_side = "top",
-      annotation_name_rot = 45,
-      annotation_legend_param = list(Sample = list(title_gp = gpar(fontsize = 18, fontface = "bold"), 
-                                                   labels_gp = gpar(fontsize = 14)),
-                                     Category = list(title_gp = gpar(fontsize = 18, fontface = "bold"), 
-                                                   labels_gp = gpar(fontsize = 14)))
+      # "Sample" = Sample,
+      # "Category" = Category,
+      "Sample" = anno_text(show_name = T,
+                           x = Sample, 
+                           location = 0.05, 
+                           just = "left",
+                           gp = gpar(fill = alpha(colour = Sample_colors[Sample],alpha =  0.5),
+                                     col = "black", border = NA),
+                           width = max_text_width(Sample)*1.1),
+      "Category" = anno_text(show_name = T,
+                             x = Category, 
+                             location = 0.05, 
+                             just = "left",
+                             gp = gpar(fill = alpha(colour = Category_colors[Category],alpha =  0.5),
+                                       col = "black", border = NA),
+                             width = max_text_width(Category)*1.1),
+      "Analyte" = anno_text(show_name = T,
+                             x = Analyte, 
+                             location = 0.05, 
+                             just = "left",
+                             gp = gpar(fill = "White",
+                                       col = "black", border = NA),
+                             width = max_text_width(Analyte)*1.1),
+      "Unit" = anno_text(show_name = T,
+                             x = Unit, 
+                             location = 0.05, 
+                             just = "left",
+                             gp = gpar(fill = alpha(colour = Unit_colors[Unit],alpha =  0.5),
+                                       col = "black", border = NA),
+                             width = max_text_width(Unit)*1.1),
+      # col = list(
+      #   "Sample" = Sample_colors,
+      #   "Category" = Category_colors
+      # ),
+      annotation_name_side = "top",
+      # annotation_legend_param = list(
+      #   Sample = list(title_gp = gpar(fontsize = 18, fontface = "bold"),
+      #                 labels_gp = gpar(fontsize = 14)),
+      #   Category = list(title_gp = gpar(fontsize = 18, fontface = "bold"), 
+      #                   labels_gp = gpar(fontsize = 14))),
+      annotation_name_rot = 0
     )
     
     
-    ht <- ComplexHeatmap::Heatmap(table_for_heatmap,
+    ht <- ComplexHeatmap::Heatmap(matrix = table_for_heatmap,
                                   name = "Value",
+                                  row_dend_reorder = F,
                                   col = color_scale,
                                   cluster_rows = F,
                                   cluster_columns = F,
                                   na_col = "White",
                                   row_title = "",
+                                  show_row_names = F,
                                   row_title_side = "left",
                                   row_names_side = "left",
                                   row_names_max_width = max_text_width(
                                     rownames(table_for_heatmap)
                                   ),
                                   column_title = "Date",
-                                  column_title_side = "bottom",
-                                  column_names_side = "bottom",
+                                  column_title_side = "top",
+                                  column_names_side = "top",
                                   left_annotation = row_ha,
                                   column_names_rot = 45, 
                                   # width = unit(15, "cm"),
@@ -169,19 +248,74 @@ server <- function(input, output,session) {
   
   output$plot_individual <- renderPlot({
     individual_analyte = d3()
+    # file <- d() %>% 
+    #   dplyr::filter(Analyte == individual_analyte) %>% 
+    #   dplyr::arrange(Date) %>% 
+    #   dplyr::mutate(Color2 = c(Color[2:length(Color)],Color[1]))
     file <- d() %>% 
-      dplyr::filter(Analyte == individual_analyte) %>% 
-      dplyr::mutate(Color2 = c(Color[2:length(Color)],Color[1]))
-    print(nrow(file))
-    ggplot(data = file,
-           mapping = aes(
-             x = Date,
-             y = Measure,
-           )) +
-      ylab(individual_analyte) +
-      geom_ribbon(data = file, mapping = aes(x=Date, y=Measure, ymax=Max, ymin=Min, group = 1), fill = "green", alpha = 0.2) +
-      geom_line(mapping = aes(group = 1, color = Color2)) +
-      geom_point(mapping = aes(color = Color), size = 5)
+      dplyr::filter(Full_Name == individual_analyte) %>% 
+      dplyr::arrange(Date)
+    if(nrow(file)>1) {
+      file$Color2 <- c(file$Color[2:length(file$Color)],file$Color[1])
+    } else {
+      file$Color2 <- file$Color
+    }
+    
+    if(nrow(file)>1) {
+      ggplot(data = file,
+             mapping = aes(
+               x = Date,
+               y = Measure,
+             )) +
+        ylab(individual_analyte) +
+        geom_ribbon(data = file, mapping = aes(x=Date, y=Measure,
+                                               ymax=Max+0.001, ymin=Min-0.001,
+                                               group = 1), fill = "#3e9e1f", alpha = 0.1) +
+        geom_line(mapping = aes(group = 1, color = Color2), size = 2) +
+        geom_point(mapping = aes(color = Color), size = 5) +
+        scale_color_manual(values = color_scale, name = "Value") +
+        theme_classic() +
+        theme(
+          axis.text = element_text(size = 14, color = "black", face = "plain"),
+          axis.title = element_text(size = 14, color = "black", face = "plain"),
+          axis.line = element_line(colour = "black", size = 0.5, linetype = "solid"),
+          panel.grid = element_blank(),
+          legend.position = "top",
+          legend.text = element_text(size = 14, color = "black", face = "plain"),
+          legend.title = element_text(size = 14, color = "black", face = "plain")
+        )
+    } else {
+      ggplot() +
+        ylab(individual_analyte) +
+        geom_point(data = file,
+                   mapping = aes(
+                     x = Date,
+                     y = Measure,
+                     color = Color
+                   ), size = 0) +
+        geom_rect(data = file, mapping = aes(xmin = 0.75,
+                                             xmax = 1.25,
+                                             ymin = Min-0.001,
+                                             ymax = Max+0.001),
+                  fill = "#3e9e1f", alpha = 0.1) +
+        geom_point(data = file,
+                   mapping = aes(
+                     x = Date,
+                     y = Measure,
+                     color = Color
+                   ), size = 5) +
+        scale_color_manual(values = color_scale, name = "Value") +
+        theme_classic() +
+        theme(
+          axis.text = element_text(size = 14, color = "black", face = "plain"),
+          axis.title = element_text(size = 14, color = "black", face = "plain"),
+          axis.line = element_line(colour = "black", size = 0.5, linetype = "solid"),
+          panel.grid = element_blank(),
+          legend.position = "top",
+          legend.text = element_text(size = 14, color = "black", face = "plain"),
+          legend.title = element_text(size = 14, color = "black", face = "plain")
+        )
+    }
   })
 }
 
